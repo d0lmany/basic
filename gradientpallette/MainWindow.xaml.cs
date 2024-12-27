dce7c7530c037e596fefe7efea516038a61a6772
d0lmany <d0lmany.is.god@gmail.com>
2024-12-27 23:12:16 +0500

    adding pallete lol

---
+++ gradientpallette/MainWindow.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace gradientpallette
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            for(byte i = 0; i < byte.MaxValue; i++)
+            {
+                for(byte j = i ; j < byte.MaxValue; j++)
+                {
+                    Rectangle p = new()
+                    {
+                        Width = 3, Height = 3, Fill = new SolidColorBrush(Color.FromRgb(j,0,0))
+                    };
+                    Board.Children.Add(p);
+                }
+            }
+        }
+    }
+}
